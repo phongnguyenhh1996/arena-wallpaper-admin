111ebee2fb2674317906efb548cab51bb09828d7
Phong Nguyen <phongnhh@Phongs-Mac-mini.local>
2020-05-19 16:44:21 +0700

    Updates

---
+++ precache-manifest.d35fd3071c344deff64c2b1b757813ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c35e32bea35f58c8af425a5224ac09e7",
+    "url": "/arena-wallpaper-admin/index.html"
+  },
+  {
+    "revision": "f967f6010861472f01d2",
+    "url": "/arena-wallpaper-admin/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8e1db07554b586b6a2de",
+    "url": "/arena-wallpaper-admin/static/js/2.aaac2316.chunk.js"
+  },
+  {
+    "revision": "74302187dc1f033042b1164070a401c8",
+    "url": "/arena-wallpaper-admin/static/js/2.aaac2316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f967f6010861472f01d2",
+    "url": "/arena-wallpaper-admin/static/js/main.4dba6b88.chunk.js"
+  },
+  {
+    "revision": "d787511942809404c772",
+    "url": "/arena-wallpaper-admin/static/js/runtime-main.2fa121a3.js"
+  }
+]);
